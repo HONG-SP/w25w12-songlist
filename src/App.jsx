ba0c42a5a60632c3e0a0e976ee6827dd66ebca6a
HONG-SP <hong359352@naver.com>
2025-11-20 14:21:52 +0900

    👽️ Backend 접근

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import SongList from './pages/SongList.jsx'
 import SongDetail from './pages/SongDetail.jsx'
+import { getSongList } from './api/songApi'
 
-const songs = [
-  {
-    "id": "69149cde71914f6a424fd013",
-    "title": "Blue Valentine",
-    "singer": "NMIXX",
-    "rating": 5,
-    "lyrics": "If this love is over, 다시 뛰어들어 난\n이 사랑은 colder, I'll keep the fire lit in mine\nYou'll always be my blue, ooh-ooh-ooh-ooh valentine\nYou'll always be my blue, ooh-ooh-ooh-ooh valentine"
-  },
-  {
-    "id": "6914a3a771914f6a424fd017",
-    "title": "CRUSH ON YOU",
-    "singer": "NMIXX",
-    "rating": 5,
-    "lyrics": "Oh I got a crush on you\n내겐 너란 favorite view\n다른 건 다 blurry 넌 only, I’m fallin’\nOh I got a crush on you\n같이 꾸고 싶은 꿈\n궁금해 your morning and evening\nCrush on, crush on, crush on you"
-  },
-  {
-    "id": "6914a3a771914f6a424fd018",
-    "title": "Phoenix",
-    "singer": "NMIXX",
-    "rating": 5,
-    "lyrics": "한 줌 흙 속에 잠겨\n차게 식었던 heart\n거친 숨결을 깨워\n불어, 새파란 spark\nRise up and wake up from the fire\n솟아올라 on the ground\n불을 지펴 다시 타올라 ah-ah-ah-ah-ah\nBreak up all the bounds\n날갤 펼쳐 to the sky\n갇혀있던 숨이 터져 막 ah-ah-ah-ah-ah"
-  },
-]
+// const songs = [
+//   {
+//     "id": "69149cde71914f6a424fd013",
+//     "title": "Golden",
+//     "singer": "HUNTR/X",
+//     "rating": 3,
+//     "lyrics": "I'm done hidin', now I'm shinin' like I'm born to be\nWe dreamin' hard, we came so far, now I'll believe\nWe're goin' up, up, up, it's our moment\nYou know together we're glowing\nGonna be, gonna be golden\nOh, up, up, up with our voices\n영원히 깨질 수 없는\nGonna be, gonna be golden"
+//   },
+//   {
+//     "id": "6914a3a771914f6a424fd017",
+//     "title": "Shine Like Stars",
+//     "singer": "LUMI",
+//     "rating": 4,
+//     "lyrics": "밤 하늘에 너를 그려\n별빛 속에 너를 비춰\n우리 둘의 시간은 빛나\n조용히 다가온 설렘\n너의 손을 꼭 잡고서\n함께 걷는 이 길 위에서\n수줍은 나의 고백\n달빛 아래 속삭임\n우리 꿈은 반짝여\n끝없이 shine like stars"
+//   },
+//   {
+//     "id": "6914a3a771914f6a424fd018",
+//     "title": "Blue Ocean",
+//     "singer": "SEAVEN",
+//     "rating": 5,
+//     "lyrics": "푸른 바다 저 끝까지\n너와 함께 떠나볼래\n출렁이는 파도 위로\n자유롭게 날아올라\n서로 닮은 미소 사이\n파도소리 속 비밀\n부드러운 바람 따라\n행복을 실어 보내\n눈부신 빛 가득해\n우리의 blue ocean"
+//   },
+// ]
 
 function App() {
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        const data = await getSongList()
+        setSongs(data)
+      } catch (err) {
+        console.error("Failed to fetch songs:", err)
+      }
+    }
+
+    fetchSongs()
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<SongList songs={songs} />} />
